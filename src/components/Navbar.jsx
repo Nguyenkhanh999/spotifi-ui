6edd37376ad40e0a623ebfa038b5cb3cbf3a9beb
Le Hong Hieu <hieulh@rikkeisoft.com>
2024-04-11 18:13:03 +0700

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Navbar() {
+  return (
+    <div className=''>
+       <i className='fa fa-spotify'>
+        Spotify
+       </i>
+    </div>
+  )
+}
+export default Navbar
